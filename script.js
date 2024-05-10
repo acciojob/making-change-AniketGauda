1e83e8091531e7ab40bbed4860fb30a06b8f8922
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-10 19:59:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const makeChange = (c) => {
+const makeChange = (n) => {
   // your name here
+	let values = [25,10,5,1];
+	let romanLetters = ["q","d","n","p"];
+	let str="";
+    for(let i=0;i<values.length;i++){
+        while(n>=values[i]){
+            n-=values[i];
+            str+=`${romanLetters[i]}`
+        }
+    }
+    let qcnt=0,dcnt=0,ncnt=0,pcnt=0;
+    for(let i=0;i<str.length;i++){
+        if(str[i]=='q')
+            qcnt++;
+        else if(str[i]=='d')
+            dcnt++;
+        else if(str[i]=='n')
+            ncnt++;
+        else if(str[i]=='p')
+            pcnt++;
+    }
+
+    let obj = {
+        "q" : qcnt,
+        "d" : dcnt,
+        "n" : ncnt,
+        "p" : pcnt,
+    }
+
+    return obj;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
+const c = prompt("Enter c: ")
 alert(JSON.stringify(makeChange(c)));
